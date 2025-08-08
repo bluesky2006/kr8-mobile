a8c80cc92aaf4253cc1bfcbf7d42abd738fde1ef
bluesky2006 <simon.busby@gmail.com>
2025-08-08 13:07:21 +0100

    initiailise react native with expo and tailwind

---
+++ kr8-reactnative/kr8-mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
