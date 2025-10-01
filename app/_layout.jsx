09f7f62491e579f2e724c2c6c62135bf5dd7635e
bluesky2006 <simon.busby@gmail.com>
2025-10-01 11:32:52 +0100

    fix name in Stack.Screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -28,7 +28,7 @@ export default function RootLayout() {
     <CurrentPlaylistProvider>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="tabs" options={{ headerShown: false }} />
         </Stack>
       </ThemeProvider>
     </CurrentPlaylistProvider>
