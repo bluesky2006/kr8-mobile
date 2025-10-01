8da15f79c1adfb1737a86bd07cbb856a961a36ce
bluesky2006 <simon.busby@gmail.com>
2025-10-01 20:49:00 +0100

    simplify getPlaylistTotalSeconds function

--- utils/getPlaylistTotalSeconds.js
+++ utils/getPlaylistTotalSeconds.js
@@ -1,6 +1,6 @@
 export function getPlaylistTotalSeconds(tracks = []) {
   return tracks.reduce((sum, track) => {
-    const num = Number(track?.track_length);
-    return Number.isFinite(num) ? sum + Math.floor(num) : sum;
+    const num = Math.floor(Number(track?.track_length) || 0);
+    return sum + num;
   }, 0);
 }
