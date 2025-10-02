bc5f84a60cc319dd3f9f404515480cb56ce12942
bluesky2006 <simon.busby@gmail.com>
2025-10-02 09:32:46 +0100

    remove redundant prop

--- components/SquareCover.jsx
+++ components/SquareCover.jsx
@@ -2,11 +2,9 @@ import PlaceholderArtwork from "@/components/PlaceholderArtwork";
 import { renderImageFromUint8 } from "@/utils/renderImageFromUint8";
 import { View } from "react-native";
 
-export default function SquareCover({ imageBytes, className = "" }) {
+export default function SquareCover({ imageBytes }) {
   return (
-    <View
-      className={`aspect-square rounded-xl overflow-hidden bg-gray-200 dark:bg-gray-800 ${className}`}
-    >
+    <View className={"aspect-square rounded-xl overflow-hidden bg-gray-200 dark:bg-gray-800"}>
       {imageBytes ? (
         renderImageFromUint8(imageBytes, {
           className: "w-full h-full",
